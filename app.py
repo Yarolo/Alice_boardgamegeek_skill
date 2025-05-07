c97b0778c944eb752b359f9a26d92884dad9f343
Admin <goinggreengreengoing@gmail.com>
2025-05-07 20:52:25 +0300

    Надежный переводчик и комментарии.

--- app.py
+++ app.py
@@ -3,20 +3,34 @@ from waitress import serve
 from flask import Flask, request, jsonify
 import logging
 from boardgames_info import findgame, game_base_info
-from http_work import skill_image_disconnect, image_to_skill_connect, request_repeat
+from http_work import skill_image_disconnect, image_to_skill_connect
 import random
+import time
+from functools import lru_cache
 
 app = Flask(__name__)
 
 logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 sessionStorage = {}
-
+GAME_CACHE = {}
+MAX_CACHE_SIZE = 100
 DICE = {1: '⚀', 2: '⚁', 3: '⚂', 4: '⚃', 5: '⚄', 6: '⚅'}
+
+QUICK_RESPONSES = {
+    'привет': lambda name: f'Привет, {name}! Чем могу помочь?',
+    'здравствуй': lambda name: f'Здравствуй, {name}! Как я могу помочь?',
+    'спасибо': 'Пожалуйста! Обращайтесь ещё!',
+    'благодарю': 'Всегда рад помочь!',
+    'пока': 'До свидания! Хорошего дня!',
+    'что ты умеешь': 'Я могу рассказать о настольных играх, бросить кубики или вытянуть карты. Что вас интересует?'
+}
 
 
 @app.route('/post', methods=['POST'])
 def main():
+    start_time = time.time()
     response = {
         'session': request.json['session'],
         'version': request.json['version'],
@@ -26,118 +40,141 @@ def main():
     }
     handle_dialog(request.json, response)
 
+    logger.info(f"Request processed in {time.time() - start_time:.3f}s")
     return jsonify(response)
 
 
 def handle_dialog(req, res):
     user_id = req['session']['user_id']
+    command = req['request']['command'].lower()
+
     if sessionStorage.get('image_id'):
         skill_image_disconnect(sessionStorage['image_id'], 'Волобуев Ярослав')
+        sessionStorage.pop('image_id', None)
+
+    for quick_cmd, response in QUICK_RESPONSES.items():
+        if quick_cmd in command:
+            if callable(response):
+                res['response']['text'] = response(sessionStorage.get(user_id, {}).get('first_name', 'друг'))
+            else:
+                res['response']['text'] = response
+            return
 
     if req['session']['new']:
         res['response']['text'] = 'Привет! Назови свое имя!'
-
-        sessionStorage[user_id] = {
-            'first_name': None
-        }
+        sessionStorage[user_id] = {'first_name': None}
         return
+
     if sessionStorage[user_id]['first_name'] is None:
         acquaintance(req, res, user_id)
     else:
-        if 'игр' in req['request']['command'] and 'настольн' in req['request']['command']:
-            res['response']['text'] = f'Супер! Информацию о какой настольной игре хочешь узнать?'
+        if 'игр' in command and 'настольн' in command:
+            res['response']['text'] = 'О какой настольной игре рассказать?'
             sessionStorage['boardgames_info'] = True
             sessionStorage['dice_pull'] = False
             sessionStorage['draw_cards'] = False
-        elif ('кост' in req['request']['command'] or 'куб' in req['request']['command']) and 'брос' in req['request'][
-            'command']:
-            res['response']['text'] = f'Сколько кубиков хотите кинуть?'
+        elif ('кост' in command or 'куб' in command) and 'брос' in command:
+            res['response']['text'] = 'Сколько кубиков кидаем?'
             sessionStorage['draw_cards'] = False
             sessionStorage['boardgames_info'] = False
             sessionStorage['dice_pull'] = True
-        elif ('вытя' in req['request']['command'] or 'результат' in req['request']['command']) and 'карт' in \
-                req['request']['command']:
-            res['response']['text'] = f'Сколько карт хотите вытянуть?'
+        elif ('вытя' in command or 'результат' in command) and 'карт' in command:
+            res['response']['text'] = 'Сколько карт вытягиваем?'
             sessionStorage['draw_cards'] = True
             sessionStorage['boardgames_info'] = False
             sessionStorage['dice_pull'] = False
-        elif sessionStorage['boardgames_info']:
+        elif sessionStorage.get('boardgames_info'):
             boardgames_dialog(req, res)
-        elif sessionStorage['dice_pull']:
+        elif sessionStorage.get('dice_pull'):
             dice_dialog(req, res)
-        elif sessionStorage['draw_cards']:
+        elif sessionStorage.get('draw_cards'):
             draw_cards(req, res)
 
 
 def boardgames_dialog(req, res):
-    try:
-        boardgames = findgame(req['request']['original_utterance'])
-    except ValueError:
-        res['response']['text'] = 'Извините, не смогла найти игру с таким именем'
+    query = req['request']['original_utterance'].lower().strip()
+
+    if query in GAME_CACHE:
+        cached = GAME_CACHE[query]
+        res['response']['text'] = cached['text']
+        if 'image' in cached:
+            im_id = image_to_skill_connect(cached['image'], 'Волобуев Ярослав')
+            if im_id:
+                res['response']['card'] = cached['card']
+                sessionStorage['image_id'] = im_id
         return
-    res['response']['text'] = game_base_info(boardgames)
-    if not isinstance(boardgames, list):
-        im_id = image_to_skill_connect(boardgames['image'], 'Волобуев Ярослав')
-        res['response']['card'] = {
-            "type": "BigImage",
-            "image_id": im_id,
-            "title": boardgames['name'],
-            "description": game_base_info(boardgames),
-        }
-        sessionStorage['image_id'] = im_id
+
+    try:
+        boardgames = findgame(query)
+        info = game_base_info(boardgames)
+        res['response']['text'] = info
+
+        if not isinstance(boardgames, list):
+            if len(GAME_CACHE) >= MAX_CACHE_SIZE:
+                GAME_CACHE.pop(next(iter(GAME_CACHE)))
+
+            GAME_CACHE[query] = {
+                'text': info,
+                'image': boardgames['image'],
+                'card': {
+                    "type": "BigImage",
+                    "image_id": "",
+                    "title": boardgames['name'],
+                    "description": info,
+                }
+            }
+
+            im_id = image_to_skill_connect(boardgames['image'], 'Волобуев Ярослав')
+            if im_id:
+                res['response']['card'] = {
+                    "type": "BigImage",
+                    "image_id": im_id,
+                    "title": boardgames['name'],
+                    "description": info,
+                }
+                sessionStorage['image_id'] = im_id
+    except Exception as e:
+        logger.error(f"Game search error: {str(e)}")
+        res['response']['text'] = 'Извините, не смогла найти информацию об этой игре'
 
 
 def acquaintance(req, res, user_id):
     first_name = get_first_name(req)
     if first_name is None:
-        res['response']['text'] = \
-            'Не расслышала имя. Повтори, пожалуйста!'
+        res['response']['text'] = 'Не расслышала имя. Повтори, пожалуйста!'
     else:
         sessionStorage[user_id]['first_name'] = first_name
-        res['response'][
-            'text'] = f'Приятно познакомиться, {first_name.title()}. Что хотите узнать?'
+        res['response']['text'] = f'Приятно познакомиться, {first_name.title()}! Чем помочь?'
         res['response']['buttons'] = [
-            {
-                'title': 'Информацию о настольной игре',
-                'hide': True
-            },
-            {
-                'title': 'Значение вытянутых карт',
-                'hide': True
-            },
-            {
-                'title': 'Результат бросков кубиков',
-                'hide': True
-            }
+            {'title': 'Информация о настольной игре', 'hide': True},
+            {'title': 'Бросить кубики', 'hide': True},
+            {'title': 'Вытянуть карты', 'hide': True}
         ]
 
 
 def dice_dialog(req, res):
-    number_dices = get_number(req)
-    result = []
-    if not number_dices:
-        res['response']['text'] = 'Не расслышала ответ. Введите число брошенных кубиков.'
+    num_dice = get_number(req)
+    if not num_dice or num_dice < 1:
+        res['response']['text'] = 'Пожалуйста, укажите число от 1 до 10'
         return
-    for i in range(number_dices):
-        result.append(DICE[random.choice(range(1, 7))])
-    result = sorted(result)
-    answer = f'Ваш результат:\n{"".join(result)}'
-    if number_dices == 5:
-        combos = []
-        for i in DICE.values():
-            combos.append(result.count(i))
-        if 5 in combos:
-            answer += '\n Комбинация из кубиков: Покер.'
-        elif 3 in combos and 2 in combos:
-            answer += '\n Комбинация из кубиков: Фулхаус.'
-        elif result == '⚀⚁⚂⚃' or result == '⚁⚂⚃⚄' or result == '⚂⚃⚄⚅':
-            answer += '\n Комбинация из кубиков: Короткий стрит.'
-        elif result == '⚁⚂⚃⚄⚅' or result == '⚀⚁⚂⚃⚄':
-            answer += '\n Комбинация из кубиков: Длинный стрит.'
-        elif 4 in combos:
-            answer += '\n Комбинация из кубиков: Каре.'
+    num_dice = min(num_dice, 10)
+    result = sorted([DICE[random.randint(1, 6)] for _ in range(num_dice)])
+    res['response']['text'] = f'Результат: {"".join(result)}'
 
-    res['response']['text'] = answer
+
+def draw_cards(req, res):
+    CARD_SUITS = {'spades': '♠', 'hearts': '♥', 'diamonds': '♦', 'clubs': '♣'}
+    CARD_VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    full_deck = [f"{value}{suit}" for suit in CARD_SUITS.values() for value in CARD_VALUES]
+
+    num_cards = get_number(req)
+    if not num_cards or num_cards < 1:
+        res['response']['text'] = "Пожалуйста, укажите число от 1 до 10"
+        return
+
+    num_cards = min(num_cards, 10)
+    drawn_cards = random.sample(full_deck, num_cards)
+    res['response']['text'] = f"Вытянутые карты: {' '.join(drawn_cards)}"
 
 
 def get_first_name(req):
@@ -150,39 +187,6 @@ def get_number(req):
     for entity in req['request']['nlu']['entities']:
         if entity['type'] == 'YANDEX.NUMBER':
             return entity.get('value', None)
-
-
-def draw_cards(req, res):
-    CARD_SUITS = {
-        'spades': '♠',
-        'hearts': '♥',
-        'diamonds': '♦',
-        'clubs': '♣'
-    }
-
-    CARD_VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-
-    full_deck = []
-    for suit in CARD_SUITS.values():
-        for value in CARD_VALUES:
-            full_deck.append(f"{value}{suit}")
-
-    num_cards = get_number(req)
-
-    if not num_cards or num_cards <= 0:
-        res['response']['text'] = "Пожалуйста, укажите положительное число карт для выбора."
-        return
-
-    if num_cards > len(full_deck):
-        res['response']['text'] = f"В колоде только {len(full_deck)} карт. Вы не можете выбрать больше."
-        return
-
-    drawn_cards = random.sample(full_deck, num_cards)
-
-    if num_cards == 1:
-        res['response']['text'] = f"Выбранная карта: {drawn_cards[0]}"
-    else:
-        res['response']['text'] = f"Выбранные карты: {' '.join(drawn_cards)}"
 
 
 if __name__ == '__main__':
--- http_work.py
+++ http_work.py
@@ -1,4 +1,19 @@
 import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+session = requests.Session()
+retry_strategy = Retry(
+    total=2,
+    backoff_factor=0.5,
+    status_forcelist=[500, 502, 503, 504]
+)
+adapter = HTTPAdapter(max_retries=retry_strategy)
+session.mount("https://", adapter)
 
 SKILLS_AUTHORS_ID = {
     'Волобуев Ярослав': 'OAuth y0__xDrrISFBhij9xMg7OK21BJ192cgMFXEUbcohgmIHSQlvuxsZA'
@@ -9,22 +24,42 @@ SKILLS_ID = {
 
 
 def image_to_skill_connect(image_url, author_name):
-    headers = {
-        'Authorization': SKILLS_AUTHORS_ID[author_name],
-        'Content-Type': 'application/json'
-
-    }
-    response = requests.post(f'https://dialogs.yandex.net/api/v1/skills/{SKILLS_ID[author_name]}/images',
-                             headers=headers,
-                             json={"url": image_url}).json()
-    return response['image']['id']
+    try:
+        headers = {
+            'Authorization': SKILLS_AUTHORS_ID[author_name],
+            'Content-Type': 'application/json'
+        }
+        response = session.post(
+            f'https://dialogs.yandex.net/api/v1/skills/{SKILLS_ID[author_name]}/images',
+            headers=headers,
+            json={"url": image_url},
+            timeout=3
+        )
+        response.raise_for_status()
+        return response.json()['image']['id']
+    except Exception as e:
+        logger.error(f"Error uploading image: {str(e)}")
+        return None
 
 
 def skill_image_disconnect(image_id, author_name):
-    headers = {'Authorization': SKILLS_AUTHORS_ID[author_name]}
-    requests.delete(f'https://dialogs.yandex.net/api/v1/skills/{SKILLS_ID[author_name]}/images/{image_id}',
-                    headers=headers)
+    try:
+        headers = {'Authorization': SKILLS_AUTHORS_ID[author_name]}
+        session.delete(
+            f'https://dialogs.yandex.net/api/v1/skills/{SKILLS_ID[author_name]}/images/{image_id}',
+            headers=headers,
+            timeout=2
+        )
+    except Exception as e:
+        logger.error(f"Error deleting image: {str(e)}")
 
 
 def request_repeat(request):
-    requests.post('https://mighty-eager-jay.glitch.me/post', json=request)
+    try:
+        session.post(
+            'https://mighty-eager-jay.glitch.me/post',
+            json=request,
+            timeout=2
+        )
+    except Exception as e:
+        logger.error(f"Error repeating request: {str(e)}")
